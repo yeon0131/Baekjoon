825a5e39ed02118e80b14bf5ae07cb74ebfb236a
yeon0131 <ksy0332@naver.com>
2024-09-07 23:53:50 +0900

    10828번(스택문제) 풀이 수정

--- 240910/_10828_stack.py
+++ 240910/_10828_stack.py
@@ -4,8 +4,33 @@
 # size: 스택에 들어있는 요소 개수 출력
 # empty: 스택이 비어있으면 1, 아니면 0 출력
 # top: 스택의 가장 위에 있는 정수 출력. 비어있을 시엔 -1 출력
+
 import sys
+
 N = int(sys.stdin.readline())
 stack_list = list(sys.stdin.readline().split())
-for _ in N :
-    if
+
+stack_box = []
+
+for ele in stack_list :
+    if "push" in ele:
+        # 공백을 기준으로 자르고 마지막 요소 반환
+        ele = ele.split()[-1]
+        stack_box.append(ele)
+
+    elif "pop" in stack_list:
+        res = stack_box.pop()
+        print(res)
+
+    elif "size" in stack_list:
+        print(len(stack_box))
+
+    elif "empty" in stack_list:
+        if not stack_box:
+            print("1")
+        print("0")
+
+    elif "top" in stack_list:
+        if not stack_box:
+            print("-1")
+        print(stack_box[-1])
