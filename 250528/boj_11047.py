a8e4365e6993771e0028c085244d8cd8ab4c0ccb
yeon0131 <ksy0332@naver.com>
2025-06-01 16:58:44 +0900

    11047번(greedy) 풀이

--- 250528/boj_11047.py
+++ 250528/boj_11047.py
@@ -2,3 +2,17 @@
 # 이때 필요한 동전 개수의 최솟값을 구하는 프로그램 맹들어라.
 # 첫째줄엔 N,K가 주어지고 둘째 줄부터 N개의 줄에 동전의 금액이 입력됨
 
+n,k = map(int,input().split())
+coins = [int(input().strip()) for _ in range(n)] #리스트 컴프리헨션
+
+# 가장 큰 동전부터 사용하기 위해 내림차순 정렬
+coins.sort(reverse=True)
+
+count = 0
+
+for coin in coins:
+    if k >= coin:
+        count += k // coin
+        k %= coin  # 남은 금액 업데이트
+
+print(count)
